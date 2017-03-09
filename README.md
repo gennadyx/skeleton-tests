336238baf3597d0b1eb3eb287eea2ccded9e97eb
Gennady Knyazkin <dev@gennadyx.tech>
2017-03-09 07:34:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# skeleton-tests
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE)
+[![Build Status][ico-travis]][link-travis]
+[![Total Downloads][ico-downloads]][link-downloads]
+
+[![Coverage Status][ico-coverage]][link-coverage]
+[![Sensiolabs Medal][ico-code-quality-sensio]][link-code-quality-sensio]
+[![Quality Score][ico-code-quality-scrutinizer]][link-code-quality-scrutinizer]
+
+Tests for gennadyx/skeleton package
+
+## Credits
+
+- [Gennady Knyazkin][link-author]
+- [All Contributors][link-contributors]
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/gennadyx/skeleton.svg?style=flat
+[ico-license]: https://img.shields.io/packagist/l/gennadyx/skeleton.svg?style=flat
+[ico-travis]: https://img.shields.io/travis/gennadyx/skeleton/master.svg?style=flat
+[ico-coverage]: https://img.shields.io/scrutinizer/coverage/g/gennadyx/skeleton.svg?style=flat
+[ico-code-quality-scrutinizer]: https://img.shields.io/scrutinizer/g/gennadyx/skeleton.svg?style=flat
+[ico-code-quality-sensio]: https://insight.sensiolabs.com/projects/8a05f05b-d1c9-40b1-8c87-5a251f712f4d/mini.png
+[ico-downloads]: https://img.shields.io/packagist/dt/gennadyx/skeleton.svg?style=flat
+
+[link-packagist]: https://packagist.org/packages/gennadyx/skeleton
+[link-travis]: https://travis-ci.org/gennadyx/skeleton
+[link-coverage]: https://scrutinizer-ci.com/g/gennadyx/skeleton/code-structure
+[link-code-quality-scrutinizer]: https://scrutinizer-ci.com/g/gennadyx/skeleton
+[link-code-quality-sensio]: https://insight.sensiolabs.com/projects/8a05f05b-d1c9-40b1-8c87-5a251f712f4d
+[link-downloads]: https://packagist.org/packages/gennadyx/skeleton
+[link-author]: http://gennadyx.tech
+[link-contributors]: https://github.com/gennadyx/skeleton/contributors
---
+++ src/Helper/FunctionalTestTrait.php
@@ -0,0 +1,171 @@
+<?php
+
+/*
+ * This file is part of the skeleton package.
+ *
+ * (c) Gennady Knyazkin <dev@gennadyx.tech>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gennadyx\Skeleton\Tests\Helper;
+
+use Gennadyx\Skeleton\CommandHandler;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+trait FunctionalTestTrait
+{
+    use EventMockAwareTrait;
+
+    private static $root;
+
+    private static $ideaPathFixture = 'tests/fixtures/.idea/project.iml';
+
+    private $testDirs;
+
+    /**
+     * @var Filesystem
+     */
+    protected $fs;
+    
+    public static function root()
+    {
+        if (null === static::$root) {
+            static::$root = realpath('.');
+        }
+        
+        return static::$root;
+    }
+
+    public static function assertEqualsDirectory(string $expected, string $actual)
+    {
+        $finder = new Finder();
+        $finder->in($expected)
+            ->ignoreDotFiles(false)
+            ->ignoreUnreadableDirs(false)
+            ->ignoreVCS(false);
+
+        foreach ($finder as $item) {
+            $target = str_replace($expected, $actual, $item->getRealPath());
+
+            if ($item->isDir()) {
+                static::assertDirectoryExists($target);
+            } elseif ($item->isFile()) {
+                static::assertFileExists($target);
+                static::assertFileEquals($item->getRealPath(), $target);
+            }
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $this->createEventMock();
+        $this->fs = new Filesystem();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function tearDown()
+    {
+        $this->event = null;
+        $this->io = null;
+        $this->fs = null;
+    }
+
+    protected function createTestDir(string $name, string $chdir, bool $createIdeaPath): string
+    {
+        $root = sprintf('%s/%s', static::root(), $name);
+        $testDir = $root.$chdir;
+        $this->fs->mkdir($testDir);
+
+        $finder = new Finder();
+        $finder
+            ->files()
+            ->in(static::root())
+            ->exclude(['.idea', $name])
+            ->ignoreDotFiles(false);
+
+        foreach ($finder as $item) {
+            if (!$this->isTestDir($root, $item->getRealPath())) {
+                $this->copyToTestDir($item, $testDir);
+            }
+        }
+
+        if ($createIdeaPath) {
+            $this->createProjectIdeaPath($root);
+        }
+
+        return $root;
+    }
+
+    protected function isTestDir(string $root, string $path): bool
+    {
+        return strpos($path, $root) === 0;
+    }
+
+    protected function copyToTestDir(SplFileInfo $fileInfo, string $testDir)
+    {
+        $target = $testDir;
+
+        if ('' !== $fileInfo->getRelativePath()) {
+            $target .= '/'.$fileInfo->getRelativePath();
+        }
+
+        $this->fs->copy(
+            $fileInfo->getRealPath(),
+            sprintf('%s/%s', $target, $fileInfo->getBasename())
+        );
+    }
+
+    protected function createProjectIdeaPath(string $root)
+    {
+        $this->fs->copy(
+            realpath(static::$ideaPathFixture),
+            sprintf('%s/.idea/%s.iml', $root, basename($root))
+        );
+    }
+
+    protected function setEnvironmentVars(string $name)
+    {
+        $vars = [
+            'vendor' => 'test_vendor',
+            'name' => 'test_'.$name,
+            'author_name' => 'Test Name',
+            'author_email' => 'test@test.com',
+            'author_website' => ''
+        ];
+
+        foreach ($vars as $k => $v) {
+            putenv(sprintf('COMPOSER_DEFAULT_%s=%s', strtoupper($k), $v));
+        }
+    }
+
+    protected function executeTest(string $name, string $chdir = '', $createIdeaPath = false)
+    {
+        $this->setEnvironmentVars($name);
+
+        $name .= '_test';
+        $rootDir = $this->createTestDir($name, $chdir, $createIdeaPath);
+
+        chdir($rootDir.$chdir);
+        CommandHandler::handle($this->event);
+
+        $output = $this->io->getOutput();
+        static::assertEquals("Build successful!\n", $output, $output);
+
+        //if (is_dir('tests/fixtures/expected/'.$name)) {
+        //    static::assertEqualsDirectory('tests/fixtures/expected/'.$name, $rootDir);
+        //}
+        //
+        //$this->fs->remove($rootDir);
+    }
+
+
+}
